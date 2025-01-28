a67837a3901b6237823d9013d6066f33bc34fcd6
JaimeGLT <jaimeguzmanlt@gmail.com>
2025-01-28 15:08:45 -0400

    error card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,21 +8,21 @@ const Card = ({ item }) => {
 
     return (
         <div className='card' >
-            <div className='images' onClick={() => {navigate(`/product/${item.documentId}`);     window.scrollTo(0, 0);}}>
-                <img src={item.img.url} alt={item.titulo} className='mainImg'/>
+            <div className='images' onClick={() => {navigate(`/product/${item?.documentId}`); window.scrollTo(0, 0);}}>
+                <img src={item?.img.url} alt={item?.titulo} className='mainImg'/>
                 {
                     item?.img2 !== null ?  
-                        <img src={item?.img2?.url} alt={item.titulo} className='secondImg'/>
+                        <img src={item?.img2?.url} alt={item?.titulo} className='secondImg'/>
                     : ''
                 }
             </div>
             <div className='info'>
                 <div>
-                    <h2>{item.titulo}</h2>
+                    <h2>{item?.titulo}</h2>
                 </div>
                 <div className='price'>
-                    <p className={item.precio_con_descuento > 0 ? 'precio-antiguo': ''}>$ {item.precio}</p>
-                    <p>{item.precio_con_descuento > 0 && '$'} {item.precio_con_descuento}</p>
+                    <p className={item?.precio_con_descuento > 0 ? 'precio-antiguo': ''}>$ {item.precio}</p>
+                    <p>{item?.precio_con_descuento > 0 && '$'} {item?.precio_con_descuento}</p>
                 </div>
             </div>
         </div>
